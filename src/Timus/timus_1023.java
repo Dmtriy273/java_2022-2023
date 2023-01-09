949ad96ac8e517adc978b09a0757e695a03fe71e
Дима <Dimarik@yandex.ru>
2023-01-09 21:28:30 +0500

    Timus

---
+++ src/Timus/timus_1023.java
@@ -0,0 +1,21 @@
+package Timus;
+
+import java.util.Scanner;
+
+public class timus_1023 {
+
+    public static void main(String[] args) {
+
+        int i = 3;
+        int a;
+
+
+        Scanner in = new Scanner(System.in);
+        a = in.nextInt();
+        while (a % i != 0) {
+            i++;
+        }
+
+        System.out.println(i - 1);
+    }
+}
